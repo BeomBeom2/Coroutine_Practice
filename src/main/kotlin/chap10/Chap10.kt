f6e4dff12e6fd982c654140c8dfe1715644c04cd
BeomBeom2 <whwjdqja20@naver.com>
2024-12-23 11:36:40 +0900

    [Feat] Ex_10_10_6 예제 구현
    No Related Issue

--- src/main/kotlin/chap10/Chap10.kt
+++ src/main/kotlin/chap10/Chap10.kt
@@ -1,9 +1,6 @@
 package org.example.chap10
 
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.yield
+import kotlinx.coroutines.*
 import org.example.util.getElapsedTime
 
 fun Ex_10_10_2() = runBlocking<Unit> {
@@ -51,5 +48,28 @@ fun Ex_10_10_4() = runBlocking<Unit> {
      * [지난 시간: 1031ms] 코루틴7 실행 완료
      * [지난 시간: 1031ms] 코루틴8 실행 완료
      * [지난 시간: 1031ms] 코루틴9 실행 완료
+     */
+}
+
+
+fun Ex_10_10_6() = runBlocking<Unit> {
+    val job = launch {
+        println("1. launch 코루틴 작업이 시작되었습니다.")
+        delay(1000L)
+        println("2. launch 코루틴 작업이 완료되었습니다.")
+    }
+
+    println("3. runBlocking 코루틴이 곧 일시 중단 되고 메인 스레드가 양보됩니다.")
+    job.join()
+
+    println("4. runBlocking 이 메인 스레드에 분배돼 작업이 다시 재개됩니다.")
+    //runBlocking 블럭내에 launch 블럭이 곧바로 실행되어서 1번이 먼저 실행 될 것 같지만,
+    //runBlocking 코루틴과 launch 코루틴은 단일 스레드인 메인 스레드에서 실행되기 때문에,
+    //하나의 코루틴이 스레드를 양보하지 않으면 다른 코루틴이 실행되지 못한다.
+    /*
+     * 3. runBlocking 코루틴이 곧 일시 중단 되고 메인 스레드가 양보됩니다.
+     * 1. launch 코루틴 작업이 시작되었습니다.
+     * 2. launch 코루틴 작업이 완료되었습니다.
+     * 4. runBlocking 이 메인 스레드에 분배돼 작업이 다시 재개됩니다.
      */
 }
