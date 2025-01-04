4708c3b02beca790bea09cd24803b6df8e42b2ba
BeomBeom2 <whwjdqja20@naver.com>
2025-01-04 15:47:44 +0900

    [Feat] UserNameRepository, UserPhoneNumberRepository, StubUserNameRepository 예제 구현
    No Related Issue

--- src/main/kotlin/chap12/Chap12.kt
+++ src/main/kotlin/chap12/Chap12.kt
@@ -41,3 +41,33 @@ class AddUseCaseTestBeforeEach{
         assertEquals(2, res)
     }
 }
+
+interface UserNameRepository {
+    fun saveUserName(id: String, name: String)
+    fun getNameByUserId(id : String) : String
+}
+
+interface UserPhoneNumberRepository{
+    fun saveUserPhoneNumber(id: String, phoneNumber: String)
+    fun getPhoneNumberByUserId(id: String) : String
+}
+
+data class UserProfile(val id : String, val name : String, val phoneNumber : String)
+
+//다른 객체와의 의존성을 가진 객체를 테스트하기 위해서는 테스트 더블이 필요하다.
+//테스트 더블 : 객체에 대한 대체물, 객체의 행동을 모방하는 객체를 만드는 데 사용한다.
+//스텁 : 미리 정의된 데이터를 반호나하는 모방 객체로 반환값이 없는 동작은 구현하지 않으며, 반환값이 있는 동작만 미리 정의된 데이터를 반환하도록 구현.
+class StubUserNameRepository : UserNameRepository {
+    private val userNameMap = mapOf<String, String> (
+        "0x1111" to "홍길동",
+        "0x2222" to "조세영",
+    )
+
+    override fun saveUserName(id: String, name: String) {
+
+    }
+
+    override fun getNameByUserId(id: String): String {
+        TODO("Not yet implemented")
+    }
+}
